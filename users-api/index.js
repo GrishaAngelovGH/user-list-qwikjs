e379774829d7cde4ede4a6ba2cc05c00699e1574
Grisha Angelov <grisha.angelov89@gmail.com>
2023-10-02 09:38:47 +0200

    users-api: implement random user generation

--- users-api/index.js
+++ users-api/index.js
@@ -3,10 +3,22 @@ import express from 'express'
 const app = express()
 const port = 3100
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const males = ['Benjamin Parker', 'Ethan Thompson', 'Alexander Reed', 'Noah Anderson', 'Samuel Mitchell']
+const females = ['Olivia Morgan', 'Sophia Campbell', 'Ava Roberts', 'Emma Reynolds', 'Isabella Foster']
+
+app.get('/user', (req, res) => {
+  const randomBoolean = Math.random() >= 0.5
+  const randomIndex = Math.floor(Math.random() * 5)
+  const id = Math.random().toString().slice(2)
+
+  const newUser = {
+    id,
+    name: randomBoolean ? males[randomIndex] : females[randomIndex]
+  }
+
+  res.json(newUser)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`the server is listening on port ${port}`)
 })
