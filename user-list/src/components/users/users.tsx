1895470f4ab61f0340ec5212ab1254e99a3683f8
Grisha Angelov <grisha.angelov89@gmail.com>
2023-10-04 14:27:18 +0200

    user-list: implement users fetching and visualization in Users component

--- user-list/src/components/users/users.tsx
+++ user-list/src/components/users/users.tsx
@@ -1,10 +1,43 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStore, useVisibleTask$ } from "@builder.io/qwik";
+
+interface User {
+  id: string;
+  name: string;
+}
 
 export default component$(() => {
+  const store = useStore({ users: [] as User[] })
+
+  /*
+    Sometimes a task needs to run only on the browser and after rendering,
+    in that case, you should use useVisibleTask$().
+    The useVisibleTask$() is similar to useTask$() but it only runs on the browser
+    and after initial rendering. useVisibleTask$() registers a hook to be executed
+    when the component becomes visible in the viewport, it will run at least once in the browser,
+    and it can be reactive and re-execute when some tracked state changes.
+  */
+  useVisibleTask$(async () => {
+    try {
+      const res = await fetch("http://localhost:3100/users")
+      const users = await res.json()
+      store.users = [...users]
+
+    } catch (err) {
+      console.log(err)
+    }
+  })
 
   return (
     <>
       <h1>Users</h1>
+      {
+        store.users.map(v => (
+          <div key={v.id}>
+            <h3>ID: {v.id}</h3>
+            <h3>Name: {v.name}</h3>
+          </div>
+        ))
+      }
     </>
   )
 })
--- users-api/index.js
+++ users-api/index.js
@@ -1,22 +1,30 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
+app.use(cors())
 const port = 3100
 
 const males = ['Benjamin Parker', 'Ethan Thompson', 'Alexander Reed', 'Noah Anderson', 'Samuel Mitchell']
 const females = ['Olivia Morgan', 'Sophia Campbell', 'Ava Roberts', 'Emma Reynolds', 'Isabella Foster']
 
-app.get('/user', (req, res) => {
-  const randomBoolean = Math.random() >= 0.5
-  const randomIndex = Math.floor(Math.random() * 5)
-  const id = Math.random().toString().slice(2)
+app.get('/users', (req, res) => {
+  const users = []
 
-  const newUser = {
-    id,
-    name: randomBoolean ? males[randomIndex] : females[randomIndex]
+  for (let i = 0; i < 5; i++) {
+    const randomBoolean = Math.random() >= 0.5
+    const randomIndex = Math.floor(Math.random() * 5)
+    const id = Math.random().toString().slice(2)
+
+    const user = {
+      id,
+      name: randomBoolean ? males[randomIndex] : females[randomIndex]
+    }
+
+    users.push(user)
   }
 
-  res.json(newUser)
+  res.json(users)
 })
 
 app.listen(port, () => {
